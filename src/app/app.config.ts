4d2c42be68f8d785618d776411fc8699b51f8482
rahulap-aot <rahul.ap@aot-technologies.com>
2024-12-09 20:21:51 -0800

    middle

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,39 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+import { SocialAuthServiceConfig} from './../../node_modules/@abacritt/angularx-social-login/socialauth.service.d';
+import { GoogleLoginProvider } from '@abacritt/angularx-social-login'
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient(),provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideAnimationsAsync()]
+  providers: [
+    provideHttpClient(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideAnimationsAsync(),
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        lang: 'en',
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              '250595977818-pljevi5ka0hf8rlilk4drlhkoutfmken.apps.googleusercontent.com',{
+                oneTapEnabled: false,
+                prompt: 'consent'
+              }
+            )
+          },
+        ],
+        onError: (err) => {
+          console.error(err);
+        }
+      } as SocialAuthServiceConfig,
+    }
+    
+  ]
 };
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import { MeterialModule } from '../../Meterial.Module';
+import { GoogleSigninButtonModule, SocialAuthService } from '@abacritt/angularx-social-login';
 
 @Component({
   selector: 'app-login',
-  imports: [MeterialModule],
+  imports: [MeterialModule, GoogleSigninButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  SocialAuthService=inject(SocialAuthService)
+
+  OnInit(): void {
+   this.SocialAuthService.authState.subscribe((user) => {
+    console.log(user)
+   })
+  }
 }
